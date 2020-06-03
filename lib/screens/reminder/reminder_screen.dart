f74fd035753f5d59313a1c799587db5faf501f4b
Damiao Candido <tower_gamer@hotmail.com>
2020-06-03 13:04:49 -0300

    Tela de Prazos - 2

--- lib/screens/reminder/reminder_screen.dart
+++ lib/screens/reminder/reminder_screen.dart
@@ -14,14 +14,14 @@ class _ReminderScreenState extends State<ReminderScreen> {
       "_id": "5ed62f1039d49b27140ccb0f",
       "number": 20,
       "subjects": "Comer puta",
-      "deadline": "2020-06-02T12:00:00.000Z",
+      "deadline": "2020-06-05T12:00:00.000Z",
     },
     {
       "done": true,
       "_id": "5ed549bb6bd0171d707b86df",
       "number": 30,
       "subjects": "Testando...",
-      "deadline": "2020-06-02T14:00:00.000Z",
+      "deadline": "2020-06-10T16:00:00.000Z",
     }
   ];
 
@@ -32,24 +32,65 @@ class _ReminderScreenState extends State<ReminderScreen> {
         title: Text("Prazos"),
       ),
       body: ListView.builder(
+        physics: BouncingScrollPhysics(),
         itemCount: list.length,
         itemBuilder: (context, index){
           return Card(
             child: Container(
-              height: 60,
+              height: 100,
               decoration: BoxDecoration(
                 color: list[index]["done"] 
-                ? Colors.red[300] 
-                : Colors.green[300]
+                ? Colors.green[300]
+                : Colors.red[300] 
               ),
               child: Column(
                 children: <Widget>[
-                  Container(
-                    child: Checkbox(
-                      value: !list[index]["done"], onChanged: null
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              child: Text(
+                                list[index]["number"].toString(),
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: Container(
+                              child: Text(
+                                list[index]["subjects"],
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ),
+                          ),
+                          Checkbox(
+                            value: list[index]["done"], onChanged: null
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                  Container()
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Text(
+                        list[index]["deadline"],
+                      ),
+                    )
+                  )
                 ],
               ),
             ),
